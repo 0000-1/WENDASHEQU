9461d1242336a9ba43656f94b92f1f7821da6b33
Lenovo <735055030@qq.com>
2021-01-30 19:49:24 +0800

    lesson 2

---
+++ src/main/java/com/example/WENDASQ/WendasqApplication.java
@@ -0,0 +1,13 @@
+package com.example.WENDASQ;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WendasqApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WendasqApplication.class, args);
+	}
+
+}
